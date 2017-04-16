7c60d62184c198841e2b50d07698243a60e9ba59
mendrugory <mendrugory@gmail.com>
2017-04-16 16:21:18 +0200

    adding code week 3

---
+++ week_03/assignment_02/quicksort/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let vec = vec![2, 3, 7, 4, 5, 1, 3, 9, 8, 6];
+    print_array(&vec)
+}
+
+fn quicksort(arr: &[i32]) -> &[i32] {
+
+    let pivot = arr.len();
+    arr
+}
+
+fn print_array(arr: &[i32]) {
+    for a in arr{
+        print!("{} ", a);
+    }
+    println!()
+}
