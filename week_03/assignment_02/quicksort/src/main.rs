e9c37c0aebb0be2b68237e04d8ced30675ae4b15
mendrugory <mendrugory@gmail.com>
2017-08-08 00:19:45 +0200

    quicksort

--- week_03/assignment_02/quicksort/src/main.rs
+++ week_03/assignment_02/quicksort/src/main.rs
@@ -1,16 +1,41 @@
+use std::clone::Clone; 
+use std::cmp::PartialOrd;
+use std::fmt::Display;
+
+
 fn main() {
-    let vec = vec![2, 3, 7, 4, 5, 1, 3, 9, 8, 6];
-    print_array(&vec)
+    let vec = vec![2, 3, 7, 4, 5, 1, 3, 9, 8, 6, 32, 43, 333, 12, 87, 90, 99, 201, 320];
+    print_array(quicksort(vec))
 }
 
-fn quicksort(arr: &[i32]) -> &[i32] {
-
-    let pivot = arr.len();
-    arr
+fn quicksort<T: Clone + PartialOrd>(vector: Vec<T>) -> Vec<T> {
+    let mut vec = vector.clone();
+    let length = vec.len();
+    if length > 1 {
+        let pivot = vec.remove(length / 2);
+        let mut first_list = vec![];
+        let mut second_list = vec![];
+        for n in vec {
+            if n > pivot {
+                second_list.push(n);
+            }
+            else{
+                first_list.push(n);
+            }
+        }
+        let mut sorted_first_list = quicksort(first_list);
+        let mut sorted_second_list = quicksort(second_list);
+        sorted_first_list.append(&mut vec![pivot]);
+        sorted_first_list.append(&mut sorted_second_list);
+        sorted_first_list
+    }
+    else{
+        vec
+    }
 }
 
-fn print_array(arr: &[i32]) {
-    for a in arr{
+fn print_array<T: Display>(vec: Vec<T>) {
+    for a in vec{
         print!("{} ", a);
     }
     println!()
